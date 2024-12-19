cc3c600b4a2152e5242f2b4a92ea9353205eeb5f
Mahavir745 <mahavir777kumar@gmail.com>
2024-12-19 07:28:16 +0530

    update responsive

--- src/component/Dashboard/DashboardHeader.jsx
+++ src/component/Dashboard/DashboardHeader.jsx
@@ -8,10 +8,10 @@ const DashboardHeader = ({userFound}) => {
     navigate('/')
   }
   return (
-    <div className='flex flex-wrap h-[100px] justify-evenly items-center bg-violet-100 text-white'>
+    <div className='flex h-[100px] justify-between gap-6 flex-wrap-reverse pl-4 pr-4 items-center bg-violet-100 text-white'>
     <p className='text-sky-800 font-semibold'>Costumer Id: <span className='text-pink-700 text-2xl textShadowEffect'>{userFound.username}</span></p>
     <p className='text-sky-800 font-semibold'>Contact No: <span className='text-pink-700 text-2xl textShadowEffect'>{userFound.contact}</span></p>
-    <button onClick={HandleLogOut} className='text-pink-700 font-semibold focus:text-sky-500'>Log Out</button>
+    <button onClick={HandleLogOut} className='text-white font-semibold focus:text-sky-500 bg-green-600 pl-4 pr-4 pt-2 pb-2 text-[14px] rounded-[8px]'>Log Out</button>
   </div>
   )
 }
