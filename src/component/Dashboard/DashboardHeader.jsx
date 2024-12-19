088d923279205a4f3f761b373aef62f76a9785fb
Mahavir745 <mahavir777kumar@gmail.com>
2024-12-19 06:48:57 +0530

    new update

--- src/component/Dashboard/DashboardHeader.jsx
+++ src/component/Dashboard/DashboardHeader.jsx
@@ -8,7 +8,7 @@ const DashboardHeader = ({userFound}) => {
     navigate('/')
   }
   return (
-    <div className='flex justify-evenly bg-yellow-400 text-white'>
+    <div className='flex flex-wrap justify-evenly bg-yellow-400 text-white'>
     <p className='text-red-800 font-semibold'>Costumer Id: <span className='text-gray-200 text-2xl textShadowEffect'>{userFound.username}</span></p>
     <p className='text-red-800 font-semibold'>Contact No: <span className='text-gray-200 text-2xl textShadowEffect'>{userFound.contact}</span></p>
     <button onClick={HandleLogOut} className='text-blue-950 font-semibold focus:text-red-500'>Log Out</button>
--- src/component/Dashboard/Statement.jsx
+++ src/component/Dashboard/Statement.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 const Statement = ({statement}) => {
   return (
-    <div className='flex justify-evenly '>
-    <p className='w-[420px] border-b-2 border-red-200 p-2 text-red-500' style={{textTransform:"capitalize"}}>{statement.expense}</p>
-    <p className='w-[180px] border bg-red-100 font-medium text-center p-2 text-red-500'>{statement.amount}</p>
+    <div className='flex justify-evenly w-[400px] sm:w-[500px] md:w-auto'>
+    <p className=' w-[300px] sm:w-[420px] border-b-2 border-red-200 p-2 text-red-500' style={{textTransform:"capitalize"}}>{statement.expense}</p>
+    <p className=' w-[100px] sm:w-[180px] border bg-red-100 font-medium text-center p-2 text-red-500'>{statement.amount}</p>
   </div>
   )
 }
